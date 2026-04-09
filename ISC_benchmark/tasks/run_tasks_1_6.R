e54332591dd97c3ccda4c3a9d25d89df7f917028
JGarnica22 <j.garnica22@gmail.com>
2026-04-09 12:45:23 +0200

    rm: old scripts

--- ISC_benchmark/tasks/run_tasks_1_6.R
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env Rscript
-
-args <- commandArgs(trailingOnly = TRUE)
-cmd_args <- c(file.path("ISC_benchmark", "run_ISC_benchmark.R"), args)
-status <- system2("Rscript", args = cmd_args)
-quit(status = status)
