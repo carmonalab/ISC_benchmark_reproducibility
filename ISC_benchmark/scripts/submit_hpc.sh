f445e6aa383a40731ea83ee92c8437ae76823720
JGarnica22 <j.garnica22@gmail.com>
2026-05-17 22:41:35 +0200

    increase resources

--- ISC_benchmark/scripts/submit_hpc.sh
+++ ISC_benchmark/scripts/submit_hpc.sh
@@ -20,11 +20,11 @@ REPO_DIR="$( git -C "$SCRIPT_DIR" rev-parse --show-toplevel )"
 LOG_DIR="$PROJECT_DIR/logs"
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 
-SLURM_PARTITION="${SLURM_PARTITION:-shared-cpu}"
+SLURM_PARTITION="${SLURM_PARTITION:-private-carmona-gpu}"
 SLURM_NODES="${SLURM_NODES:-1}"
-SLURM_CPUS="${SLURM_CPUS:-16}"
-SLURM_MEM="${SLURM_MEM:-256G}"
-SLURM_TIME="${SLURM_TIME:-12:00:00}"
+SLURM_CPUS="${SLURM_CPUS:-8}"
+SLURM_MEM="${SLURM_MEM:-512G}"
+SLURM_TIME="${SLURM_TIME:-48:00:00}"
 
 DATASET_TASKS=(missclassify SplitCelltype Nct cellular_complexity Nsamples NCell)
 CROSS_DATASET_TASKS=(batch_effects biological_perturbations)
