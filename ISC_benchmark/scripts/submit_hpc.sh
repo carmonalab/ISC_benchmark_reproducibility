45bb301686193bbdfd59ec2c1475d1faaa28f05a
JGarnica22 <j.garnica22@gmail.com>
2026-05-19 19:18:51 +0200

    increase mem

--- ISC_benchmark/scripts/submit_hpc.sh
+++ ISC_benchmark/scripts/submit_hpc.sh
@@ -22,8 +22,8 @@ TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 
 SLURM_PARTITION="${SLURM_PARTITION:-public-bigmem}"
 SLURM_NODES="${SLURM_NODES:-1}"
-SLURM_CPUS="${SLURM_CPUS:-8}"
-SLURM_MEM="${SLURM_MEM:-750G}"
+SLURM_CPUS="${SLURM_CPUS:-4}"
+SLURM_MEM="${SLURM_MEM:-950G}"
 SLURM_TIME="${SLURM_TIME:-48:00:00}"
 
 DATASET_TASKS=(missclassify SplitCelltype Nct cellular_complexity Nsamples NCell)
