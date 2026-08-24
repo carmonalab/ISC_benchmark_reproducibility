01b1e6ecc3d12c245f4fe936879d23d4062e0517
JGarnica22 <j.garnica22@gmail.com>
2026-08-24 14:28:21 +0200

    init

---
+++ sample_agnostic_utils/scshc.R
@@ -0,0 +1,40 @@
+## Installtion
+#https://github.com/igrabski/sc-SHC
+#devtools::install_github("igrabski/sc-SHC")
+
+# Returns a unique value per annotation, cannot return one for each cell type
+run_scSHC <- function(scTypeEval,
+                      ident = NULL,
+                      var.genes = NULL,
+                      parallel = TRUE,
+                      cores = 2){
+   
+   if(class(scTypeEval) != "scTypeEval"){
+      stop("input should be an scTypeEval object created with `create_scTypeEval()`")
+   }
+   
+   if(is.null(ident)){
+      ident <- scTypeEval@data$`single-cell`@ident[[1]]
+   }
+   
+   if(is.null(var.genes)){
+      var.genes <- scTypeEval@gene_lists$HVG
+   }
+   
+   data <- scTypeEval:::get_filtered_raw_matrix(scTypeEval)$counts
+   
+   
+   eval_cluster <- scSHC::testClusters(data = data,
+                                       cluster_ids = as.character(ident),
+                                       batch = NULL,
+                                       var.genes = var.genes,
+                                       alpha = 0.05,
+                                       parallel = parallel,
+                                       cores = cores,
+                                       num_PCs = 30
+   )
+   
+   ret <- mclust::adjustedRandIndex(eval_cluster[[1]], as.character(ident))
+   
+   return(ret)
+}
