a8a7cc3a6b5cb5b123e196abe8952d040fbf6700
JGarnica22 <j.garnica22@gmail.com>
2026-09-11 17:26:46 +0200

    fix: change time

--- ISC_benchmark/scripts/submit_hpc.sh
+++ ISC_benchmark/scripts/submit_hpc.sh
@@ -20,11 +20,11 @@ REPO_DIR="$( git -C "$SCRIPT_DIR" rev-parse --show-toplevel )"
 LOG_DIR="$PROJECT_DIR/logs"
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 
-SLURM_PARTITION="${SLURM_PARTITION:-public-bigmem}"
+SLURM_PARTITION="${SLURM_PARTITION:-public-cpu}"
 SLURM_NODES="${SLURM_NODES:-1}"
-SLURM_CPUS="${SLURM_CPUS:-4}"
-SLURM_MEM="${SLURM_MEM:-950G}"
-SLURM_TIME="${SLURM_TIME:-48:00:00}"
+SLURM_CPUS="${SLURM_CPUS:-8}"
+SLURM_MEM="${SLURM_MEM:-475G}"
+SLURM_TIME="${SLURM_TIME:-72:00:00}"
 
 DATASET_TASKS=(missclassify SplitCelltype Nct cellular_complexity Nsamples NCell)
 CROSS_DATASET_TASKS=(batch_effects biological_perturbations)
